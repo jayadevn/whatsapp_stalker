7458db7fb1877c38627f53f08e063c625abcda2f
supernova13892 <saranshdhingra92@gmail.com>
2018-08-24 00:47:45 +0530

    Made the working more stable, converted the code to be callback friendly instead of depending on timeouts

--- js/inject.js
+++ js/inject.js
@@ -10,6 +10,7 @@ jQuery(document).ready(function($){
 		}
 
 		window.profiles=users.split(",");
+		window.cur_profile=profiles[0];
 		iteration();
 	});
 });
@@ -35,42 +36,52 @@ function init(){
 	//add an event listener
 }
 
-// var interval=setInterval(iteration,3000);
-
+//tick function
 function iteration(){
-	$.each(profiles,function(key,val){
-		setTimeout(function(){
-			var contact_span=$("span[dir='auto']._1wjpf[title='"+val+"']");
-			if(contact_span.length){
-				var contact=contact_span.parents("._2EXPL");
-				open_contact(contact[0]);
-				setTimeout(() => {
-					var cur_time=new Date();
-					if(is_online(contact))
-						console.log(val + " was online at "+cur_time);
-					// else
-					// 	console.log("")
-				}, 100);
-			}
-		},key*2000);
-	});
+	// console.log("iteration:"+cur_profile);
+	var contact_span=$("span[dir='auto']._1wjpf[title='"+window.cur_profile+"']");
+	if(contact_span.length){
+		var contact=contact_span.parents("._2EXPL");
+		open_contact(contact[0],function(){
+			setTimeout(() => {
+				var cur_time=new Date();
+				if(check_online(function(online_status){
+					if(online_status)
+						console.log(cur_profile + " was online at "+cur_time);
 
-	setTimeout(iteration,(profiles.length+1)*2000);
+					//change the cur_profile to the next one in the list
+					var i=profiles.indexOf(cur_profile),
+						timeout=(i===profiles.length-1)?2000:200;
+					cur_profile=(i===profiles.length-1)?profiles[0]:profiles[i+1];
+					setTimeout(iteration,timeout);
+				}));
+			}, 100);
+		});
+	}
 }
 
 
 //jquery events won't work
 //need to use to Native JS events
-function open_contact(contact){
+function open_contact(contact,callback){
 	var mouse_evt= document.createEvent('MouseEvents');
     mouse_evt.initEvent('mousedown', true, true);
-    contact.dispatchEvent(mouse_evt);
+	contact.dispatchEvent(mouse_evt);
+	setTimeout(function(){
+		callback();
+	},200);
 }
 
-function is_online(){
-	var online_span=document.getElementById("main").querySelector("[title='online'].O90ur");
-	if(online_span)
-		return true;
+function check_online(callback){
 
-	return false;
+	//if the status has not been loaded yet, please wait for it by calling this function again in 500 ms
+	if($("#main").find(".O90ur[title='click here for contact info']").length){
+		setTimeout(function(){
+			check_online(callback);
+		},200);
+	}
+	else{
+		var online_span=$("#main").find(".O90ur[title='online']").length;
+		callback(online_span);
+	}
 }
