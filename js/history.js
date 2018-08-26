eb338a79bd0c0634bfa7ebdde2a72116ec8f5fe4
supernova13892 <saranshdhingra92@gmail.com>
2018-08-27 00:46:48 +0530

    Cleaned Up History page a bit, still a long way to go!

--- js/history.js
+++ js/history.js
@@ -1,3 +1,28 @@
+var profiles_history=[];
+jQuery(document).ready(function($){
+	$("body").on("click",".view_times_btn",function(e){
+		var online_times="",
+			user_name=$(this).attr("data-user");
+
+		$.each(profiles_history,function(index,user){
+			if(user.name==user_name){
+				online_times+="<table class='table'>";
+				online_times+="<tr><td>From</td><td>To</td></tr>";
+				$.each(user.online_times,function(key,times){
+					online_times+="<tr><td>"+get_time_str(times.from)+"</td><td>"+get_time_str(times.to)+"</td></tr>";
+				});
+			}
+		});
+			
+		swal({
+			title:user_name+" Online Times",
+			type:'info',
+			html:online_times
+		});
+	});
+});
+
+
 chrome.storage.sync.get(['profiles_history'],function(result){
 	console.log(result);
 	if(result.profiles_history===undefined || !result.profiles_history.length){
@@ -7,15 +32,9 @@ chrome.storage.sync.get(['profiles_history'],function(result){
 		profiles_history=result.profiles_history;
 		var html="";
 		$.each(profiles_history,function(index,user){
-			html+="<div class='user'>"
-				html+="<div class='name'>"+user.name+" <span class='view_times_btn'>View Times</span></div>";
-				html+="<div class='online_times'>";
-					$.each(user.online_times,function(key,times){
-						html+="<div>";
-							html+="From: "+get_time_str(times.from)+" To: "+get_time_str(times.to);
-						html+="</div>";
-					});
-				html+="</div>";
+			html+="<div class='user col-12'>"
+				html+="<span class='name'>"+user.name+" </span>";
+				html+="<span class='view_times_btn' data-user='"+user.name+"' >View Times</span>";
 			html+="</div>";
 		});
 		$("#users_data").html(html);
